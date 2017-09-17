c8b9342a54469ae200e9ae4ac0b4ee6bc48f2ecc
Julia Chen <juliachenjing@gmail.com>
2017-09-17 14:48:51 -0700

    sort

---
+++ Lesson7_ServiceLayerAndAOP/exercise06_2-BankApplicationAOP_question/src/main/java/cs544/spring/bank/dao/IAccountDAO.java
@@ -0,0 +1,15 @@
+package cs544.spring.bank.dao;
+
+import java.util.Collection;
+
+import cs544.spring.bank.domain.Account;
+
+public interface IAccountDAO {
+	public void saveAccount(Account account);
+
+	public void updateAccount(Account account);
+
+	public Account loadAccount(long accountnumber);
+
+	public Collection<Account> getAccounts();
+}
