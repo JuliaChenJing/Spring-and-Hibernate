0e9b46e553f02af42430db36a64f94ab20f8ee83
Julia Chen <juliachenjing@gmail.com>
2017-09-18 16:29:44 -0700

    sort

--- 2/exercise02-1-solution/exercise02-1/src/main/java/edu/mum/hw2/control/Application.java
+++ 2/exercise02-1-solution/exercise02-1/src/main/java/edu/mum/hw2/control/Application.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.Collection;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
