e0e902bb3c906b35ec61557a56fac46070f2b8dd
Julia Chen <juliachenjing@gmail.com>
2017-09-18 15:02:15 -0700

    test

--- 1 HibernateDemo/Lesson01.2-hibernate-jpa/src/main/java/edu/mum/cs544/l01/Application.java
+++ 1 HibernateDemo/Lesson01.2-hibernate-jpa/src/main/java/edu/mum/cs544/l01/Application.java
@@ -11,77 +11,88 @@ import org.hibernate.cfg.Configuration;
 import edu.mum.cs544.l01.model.Person;
 
 public class Application {
-	private static final SessionFactory sessionFactory;
-	static {
-		try {
-			Configuration configuration = new Configuration();
-			
-			// This step will read hibernate.cfg.xml
-			sessionFactory = configuration.configure().buildSessionFactory(); 
+    private static final SessionFactory sessionFactory;
+    static {
+        try {
+            Configuration configuration = new Configuration();
+            
+            // This step will read hibernate.cfg.xml
+            sessionFactory = configuration.configure().buildSessionFactory(); 
 
-		} catch (Throwable ex) {
-			System.err.println(ex);
-			throw new ExceptionInInitializerError(ex);
-		}
-	}
+        } catch (Throwable ex) {
+            System.err.println(ex);
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
 
-	public static void main(String[] args) {
-		Session session = null;
-		Transaction tx = null;
-		
-		try {
-			session = sessionFactory.openSession();
+    public static void main(String[] args) {
+        Session session = null;
+        Transaction tx = null;
+        
+        try {
+            session = sessionFactory.openSession();
 
-			// Create new instance of Person and set values to it
-			Person person = new Person();
-			person.setFirstname("George");
-			person.setLastname("Washington");
+            // Create new instance of Person and set values to it
+            Person person = new Person();
+            person.setFirstname("George");
+            person.setLastname("Washington");
 
-			tx = session.beginTransaction();
-			// save the person
-			session.persist(person);
-			tx.commit();
-			output("get ID from detached bean : " + person.getId());
+            Person bimal = new Person();
+            bimal.setFirstname("Bimal");
+            bimal.setLastname("Parajuli");
 
-		} catch (HibernateException e) {
-			System.err.println(e);
-			if (tx != null) tx.rollback();
-		} finally {
-			if (session != null) session.close();
-		}
-		
-		// ANOTHER SESSION
-		try {
-			session = sessionFactory.openSession();
-			tx = session.beginTransaction();
+            Person julia = new Person();
+            julia.setFirstname("Julia");
+            julia.setLastname("Chen");
+            
+            tx = session.beginTransaction();
+            // save the person
+            session.persist(person);
+            session.persist(bimal);
+            session.persist(julia);
+            
+            tx.commit();
+            output("get ID from detached bean : " + person.getId());
 
-			// retrieve all
-			@SuppressWarnings("unchecked")
-			List<Person> persons = 
-				session.createQuery("from Person pe order by pe.lastname desc").list();
+        } catch (HibernateException e) {
+            System.err.println(e);
+            if (tx != null) tx.rollback();
+        } finally {
+            if (session != null) session.close();
+        }
+        
+        // ANOTHER SESSION
+        try {
+            session = sessionFactory.openSession();
+            tx = session.beginTransaction();
 
-			for (Person p : persons) {
-				output("First name=" + p.getFirstname()
-						+ ", Last name= " + p.getLastname());
-			}
+            // retrieve all
+            @SuppressWarnings("unchecked")
+            List<Person> persons = 
+                session.createQuery("from Person person order by person.lastname desc").list();
 
-			tx.commit();
+            for (Person p : persons) {
+                output("First name=" + p.getFirstname()
+                        + ", Last name= " + p.getLastname());
+            }
 
-		} catch (HibernateException e) {
-			System.err.println(e);
-			if (tx != null)  tx.rollback();
-		} finally {
-			if (session != null) session.close();
-		}
-		
-		if (!sessionFactory.isClosed()) {
-			sessionFactory.close();
-		}
-	}
+            tx.commit();
 
-	private static void output(String output) {
-		System.out.println("================= OUTPUT =================");
-		System.out.println(output);		
-		System.out.println("==========================================");
-	}
+        } catch (HibernateException e) {
+            System.err.println(e);
+            if (tx != null)  tx.rollback();
+        } finally {
+            if (session != null) session.close();
+        }
+        
+        if (!sessionFactory.isClosed()) {
+            sessionFactory.close();
+        }
+    }
+
+    private static void output(String output) {
+        System.out.println("================= OUTPUT =================");
+        System.out.println(output);     
+        System.out.println("==========================================");
+    }
 }
