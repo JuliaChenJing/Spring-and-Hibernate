2982c1bf9040d7faf9942b8f84d9b1a108d2e954
Julia Chen <juliachenjing@gmail.com>
2017-11-24 09:02:15 +0800

    Update App.java

--- 6  AOP/exercise06_1-BasicSpringAOP_solution/src/main/java/cs544/spring/customers/App.java
+++ 6  AOP/exercise06_1-BasicSpringAOP_solution/src/main/java/cs544/spring/customers/App.java
@@ -5,6 +5,15 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class App {
 	public static void main(String[] args) {
+ /*
+   The ApplicationContext is the central interface within a Spring application for providing configuration information to the application. It is read-only at run time, but can be reloaded if necessary and supported by the application. A number of classes implement the ApplicationContext interface, allowing for a variety of configuration options and types of applications.
+   The ApplicationContext provides:
+  ● Bean factory methods for accessing application components.
+  ● The ability to load file resources in a generic fashion.
+  ● The ability to publish events to registered listeners.
+  ● The ability to resolve messages to support internationalization.
+  ● Inheritance from a parent context.
+  */
 		ApplicationContext context = new ClassPathXmlApplicationContext(
 				"springconfig.xml");
 		ICustomerService customerService = context.getBean("customerService",
